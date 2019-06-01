0888baec34fde2c69987edf3732c1d5d50beb910
rutvikmavani <rutvikmavani@gmail.com>
2019-06-01 13:29:57 +0530

    a

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 app.use('/groups',require('./routes/groups'));
 app.use('/quiz',require('./routes/quiz'));
-
+app.use('/categories',require('./routes/category'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started on Port ${PORT}`));
